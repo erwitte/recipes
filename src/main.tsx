d3cde8a3e21f98f163836efe4d22ff79eb4f36a0
Erik Witte <witte.erik2@gmail.com>
2026-01-21 23:16:38 +0100

    added tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './aws-exports';
+import './aws-export';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
