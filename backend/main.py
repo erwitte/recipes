cd455233ebe34508901f6712076ec7a574494c9e
erik witte <witte.erik2@gmail.com>
2026-03-18 20:05:27 +0100

    added pg docker-compose

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.post("/login")
+def login():
+    return {"message": "Login successful"}
+
+@app.post("/register")
+def register():
+    return {"message": "Registration successful"}
