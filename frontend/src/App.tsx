03b26b03220e796bc23ca134eae0998e230b905e
erik witte <witte.erik2@gmail.com>
2026-03-25 14:48:29 +0100

    fixed taildwind bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './routes/Register'
 import Login from './routes/Login'
-
 import './index.css'
 
 function App() {
@@ -9,7 +8,12 @@ function App() {
   return (
     <>
      <BrowserRouter>
-     <div>
+     <div className="min-h-screen 
+                      w-full 
+                      bg-slate-950 
+                      flex 
+                      justify-center 
+                      items-center">
       <Routes>
         <Route path="/" element={<Login />}/>
         <Route path="/register" element={<Register />} />
