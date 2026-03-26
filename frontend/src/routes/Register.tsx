cff70c6719d4aeaca0f36ae79d9b218a25c66eae
erik witte <witte.erik2@gmail.com>
2026-03-26 23:12:04 +0100

    fixed gaps in registration form

--- frontend/src/routes/Register.tsx
+++ frontend/src/routes/Register.tsx
@@ -18,21 +18,32 @@ function Register(){
     return(
         <div className="flex flex-col gap-4 w-full max-w-sm">
             <h2 className="text-2xl font-bold text-gray-800 text-center mb-2">Registrierung</h2>
+            <label className="flex flex-col gap-1">
         <span>Vorname: </span>
         <input placeholder="Vorname eingeben..." type="text" onChange={(e) => setFirstName(e.target.value)}
         className="border border-black rounded-l"/>
+        </label>
+
+        <label className="flex flex-col gap-1">
         <span>E-Mail: </span>
         <input placeholder="E-Mail eingeben..." type="text" onChange={(e) => setEmail(e.target.value)}
         className="border border-black rounded-l"/>
+        </label>
+
+        <label className="flex flex-col gap-1">
         <span>Passwort: </span>
         <input placeholder="Passwort eingeben..." type="password" onChange={(e) => setPassword(e.target.value)}
         className="border border-black rounded-l"/>
+        </label>
+
+        <label className="flex flex-col gap-1">
         <span>Passwort wiederholen: </span>
         <input placeholder="Passwort wiederholen..." type="password" onChange={(e) => {
             const repeatedPassword: string = e.target.value
             checkPasswords(repeatedPassword)
         }}
         className="border border-black rounded-l"/>
+        </label>
             <p className={clsx(
             "text-red-500 text-sm mt-1 transition-opacity",
             passwordError ? "opacity-100" : "opacity-0"
