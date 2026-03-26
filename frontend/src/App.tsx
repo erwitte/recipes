2f3f5978ae4aeb9ca000a91b31a8329c7bf0338b
erik witte <witte.erik2@gmail.com>
2026-03-26 13:12:02 +0100

    initial design done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,13 +9,14 @@ function App() {
     <>
      <BrowserRouter>
      <div 
-  className="absolute inset-0 bg-[url(https://images.pexels.com/photos/14853728/pexels-photo-14853728.jpeg)] bg-[length:100%] bg-center bg-no-repeat"
->
-      <Routes>
-        <Route path="/" element={<Login />}/>
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" />
-      </Routes>
+  className="absolute inset-0 bg-[url(https://images.pexels.com/photos/14853728/pexels-photo-14853728.jpeg)] bg-[length:100%] bg-center bg-no-repeat flex items-center justify-center">
+      <div className="bg-white/60 w-[85%] h-[85%] rounded-xl backdrop-blur-md overflow-auto p-8">
+        <Routes>
+          <Route path="/" element={<Login />}/>
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" />
+        </Routes>
+      </div>
      </div>
      </BrowserRouter>
     </>
