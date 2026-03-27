950ff2c3438db665babbb7f638599314c9f316cd
erik witte <witte.erik2@gmail.com>
2026-03-27 11:28:41 +0100

    first fetch for collections mocked

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,8 +1,29 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+type Collection = {
+    name: string,
+    image_url: string,
+    link: string
+}
+
+
 function Home(){
+    const [collections, setCollections] = useState<Collection[]>([])
+
+    useEffect(() => {
+        axios.get<Collection[]>("localhost:8000/collection")
+        .then(response => setCollections(response.data))
+        .catch(error => console.error(error));
+    }, []);
+    
+
     return (
-        <>
-        <p>hell nah</p>
-        </>
+        <ul>
+            {collections.map(collection => (
+                <li key={collection.link}>{collection.name}</li>
+            ))}
+        </ul>
     )
 }
 
