d588451e8d300b56c44302d01317f25f0b86e9c1
erik witte <witte.erik2@gmail.com>
2026-05-16 16:41:07 +0200

    basic auth done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,27 +3,52 @@ import Register from './routes/Register'
 import Login from './routes/Login'
 import Home from './routes/Home'
 import './index.css'
+// 1. Import the standard Clerk control components
+import { SignedIn, SignedOut } from '@clerk/clerk-react'
 
 function App() {
-
   return (
     <>
-     <BrowserRouter>
-     <div 
-  className="absolute inset-0 
-  bg-[url(https://images.pexels.com/photos/14853728/pexels-photo-14853728.jpeg)] 
-  bg-[length:100%] bg-center bg-no-repeat flex items-center justify-center">
-      <div className="bg-white/60 w-[85%] h-[85%] rounded-xl backdrop-blur-md overflow-auto p-8
-      flex items-center justify-center flex-col gap-6
-      border border-white/30 shadow-[0_20px_50px_rgba(0,0,0,0.3)]">
-        <Routes>
-          <Route path="/" element={<Login />}/>
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home/>}/>
-        </Routes>
-      </div>
-     </div>
-     </BrowserRouter>
+      <BrowserRouter>
+        <div className="absolute inset-0 bg-[url(https://images.pexels.com/photos/14853728/pexels-photo-14853728.jpeg)] bg-[length:100%] bg-center bg-no-repeat flex items-center justify-center">
+          <div className="bg-white/60 w-[85%] h-[85%] rounded-xl backdrop-blur-md overflow-auto p-8 flex items-center justify-center flex-col gap-6 border border-white/30 shadow-[0_20px_50px_rgba(0,0,0,0.3)]">
+            
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+
+              <Route 
+                path="/login" 
+                element={
+                  <SignedOut>
+                    <Login />
+                  </SignedOut>
+                }
+              />
+              <Route 
+                path="/register" 
+                element={
+                  <SignedOut>
+                    <Register />
+                  </SignedOut>
+                } 
+              />
+
+              <Route 
+                path="/dashboard" 
+                element={
+                  <SignedIn>
+                    <div>Welcome to your private dashboard!</div>
+                  </SignedIn>
+                }
+              />
+            </Routes>
+
+          </div>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string;
+
+function RootLayout() {
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RootLayout />
   </StrictMode>,
 )
